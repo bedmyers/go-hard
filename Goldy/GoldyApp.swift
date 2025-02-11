75a78cce75aab403d2fe0099d6d1949e6975c3a7
Blair Myers <bedmyers@gmail.com>
2025-02-10 23:12:46 -0500

    Initial Commit

---
+++ Goldy/GoldyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldyApp.swift
+//  Goldy
+//
+//  Created by Blair Myers on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GoldyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
